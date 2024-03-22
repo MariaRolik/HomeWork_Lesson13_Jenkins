9a1a731a042687999141ab6c6294560c2d3eef3b
Maria Rolik <mary.rolik@gmail.com>
2024-03-22 23:14:09 +0300

    change Configuration.remote

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.BeforeAll;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     }
 }
